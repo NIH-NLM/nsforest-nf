cdf46ab9bb30ead7b6a7c0d9059da37600501a83
Anne Deslattes Mays <adeslat@scitechcon.org>
2025-12-22 18:40:13 +0000

    added for cloudos testing

---
+++ scripts/download_pancreas.sh
@@ -0,0 +1,7 @@
+curl https://datasets.cellxgene.cziscience.com/72e6a74d-8804-4305-81d7-a12b93f2ac0d.h5ad > pancreas_Xu_2023_72e6a74d-8804-4305-81d7-a12b93f2ac0d.h5ad
+curl https://datasets.cellxgene.cziscience.com/00d88707-e33a-4c2a-821a-cdc32a98d050.h5ad > pancreas_Tritschler_2022_00d88707-e33a-4c2a-821a-cdc32a98d050.h5ad
+curl https://datasets.cellxgene.cziscience.com/111d6e7d-d3d2-48fd-907a-4d3f8c77ee93.h5ad > pancreas_Fasolino_2022_111d6e7d-d3d2-48fd-907a-4d3f8c77ee93.h5ad
+curl https://datasets.cellxgene.cziscience.com/4be7951a-4ae7-4a81-86f1-e2649b304d1c.h5ad > pancreas_TSConsortium_2022_4be7951a-4ae7-4a81-86f1-e2649b304d1c.h5ad
+curl https://datasets.cellxgene.cziscience.com/635a80e4-c8fc-4324-980f-4019e84aa067.h5ad > pancreas_Enge_2017_635a80e4-c8fc-4324-980f-4019e84aa067.h5ad
+curl https://datasets.cellxgene.cziscience.com/e4218427-f7ce-4506-a8fd-40607a652149.h5ad > pancreas_Muraro_2016_e4218427-f7ce-4506-a8fd-40607a652149.h5ad
+curl https://datasets.cellxgene.cziscience.com/74c3403a-451c-4a62-84e0-d8a8e45c7ea7.h5ad > pancreas_Han_2020_74c3403a-451c-4a62-84e0-d8a8e45c7ea7.h5ad
